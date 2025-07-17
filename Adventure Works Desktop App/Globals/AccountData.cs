6e8887e473ed0bd8963eca6e26a8d6aaf04a7af2
Tyler Ross <tross@herculesfreight.com>
2025-07-17 09:40:42 -0400

    Added comments to Account Data

--- Adventure Works Desktop App/Globals/AccountData.cs
+++ Adventure Works Desktop App/Globals/AccountData.cs
@@ -1,17 +1,14 @@
-﻿using Microsoft.SqlServer.Server;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Permissions;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Adventure_Works_Desktop_App.Globals
 {
+    /// <summary>
+    /// Account Data is used to store account data. Is used in Login and Signup. 
+    /// </summary>
     public class AccountData
     {
+        // Properties
         public string FirstName
         { get; set; }
 
@@ -30,6 +27,15 @@ namespace Adventure_Works_Desktop_App.Globals
         public string Email
         { get; set; }
 
+        /// <summary>
+        /// A constructor to ensure all portions of Account Data is filled. Mainly used for Signup.
+        /// </summary>
+        /// <param name="firstName">A given user's first name</param>
+        /// <param name="lastName">A given user's last name</param>
+        /// <param name="username">A given user's username</param>
+        /// <param name="password">A given user's password</param>
+        /// <param name="displayName">A given user's display name</param>
+        /// <param name="email">A given user's email</param>
         public AccountData(string firstName, string lastName, string username, string password, string displayName, string email) 
         {
             FirstName = firstName;
